2adfbe4b1b7ec8884f9fd2bb90f5d72d66bc5502
kyr0 <info@aron-homberg.de>
2019-10-26 19:46:55 +0200

    chore: Initial commit

---
+++ src/st-rm-rf.ts
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const promisify = require('util').promisify;
+const spawn = require("cross-spawn");
+import * as path from 'path';
+const os = require("os");
+
+const fsp = {
+    lstat: promisify(fs.lstat)
+};
+
+export const removePathOrFile = async (deletePath: string, printError: boolean = false): Promise<boolean> => {
+    try {
+        const platform = os.platform();
+        if (platform === "win32") {
+            const pathWin = path.win32;
+            deletePath = pathWin.resolve(deletePath);
+            const stat = await fsp.lstat(deletePath);
+            if (stat.isDirectory()) {
+                spawn.sync(process.env.comspec, ['/c','rmdir','/s', '/q', deletePath], {stdio: 'inherit'});
+            } else {
+                spawn.sync(process.env.comspec, ['/c', 'del', deletePath], {stdio: 'inherit'});
+            }
+            return true;
+        } else {
+            spawn.sync('rm', ['-rf', deletePath], {stdio: 'inherit'});
+            return true;
+        }
+    } catch (err) {
+        if (printError) {
+            console.log(`Error removing: ${err}`);
+        }
+        return false;
+    }
+};
