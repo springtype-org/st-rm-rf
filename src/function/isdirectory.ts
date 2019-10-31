7c5dd0b9b8ebf4f2307a24f94cb755e8f11e6d57
Michael Mannseicher <m.mansi1@gmx.net>
2019-11-01 00:21:50 +0100

    fix(naming): fix windows bug for linux

--- src/function/isdirectory.ts
+++ src/function/isdirectory.ts
@@ -1,5 +1,5 @@
 import {lstatSync, PathLike} from "fs";
-import {existPath} from "./existPath";
+import {existPath} from "./existpath";
 
 export const isDirectory = (path: PathLike) => {
     return existPath(path) && lstatSync(path).isDirectory();
