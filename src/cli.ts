0db705eb6642e557e33c97e4584555122fb72e05
mansi1 <m.mansi1@gmx.net>
2020-01-27 16:31:21 +0100

    chore:

--- src/cli.ts
+++ src/cli.ts
@@ -9,7 +9,7 @@ const pathsToDelete = process.argv.slice(2);
         console.log("[*] Nothing to delete.");
     } else {
         const length = pathsToDelete.length;
-        console.log(chalk.cyan(`'Deleting ${length==1?'path':'paths'}:  [${pathsToDelete.join(',')}]`));
+        console.log(chalk.cyan(`Deleting ${length==1?'path':'paths'}: [${pathsToDelete.map(v => chalk.white(v)).join(',')}]`));
         for (let i = 0; i < length; i++) {
             const pathToDelete = pathsToDelete[i];
             deletePathOrFile(pathToDelete, {printInfo: false, printError: true, printWarning: false});
