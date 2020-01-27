3ffc75995c2870c415451d59a70158ea7d8d0432
mansi1 <m.mansi1@gmx.net>
2020-01-27 13:05:38 +0100

    chore: delete loading ci

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,18 @@
 #!/usr/bin/env node
 import chalk from "chalk";
-import { deletePathOrFile } from "./function/st-rm-rf";
-import * as loading from 'loading-cli';
+import {deletePathOrFile} from "./function/st-rm-rf";
 
 const pathsToDelete = process.argv.slice(2);
 
 (async () => {
-  if (pathsToDelete.length == 0) {
-    console.log("[*] Nothing to delete.");
-  } else {
-    const length = pathsToDelete.length;
-    console.log(chalk.cyan('Deleting paths: ')+'['+pathsToDelete.join(',') +']') ;
-     const loader = loading("").start();
-    for (let i = 0; i < length; i++) {
-      const pathToDelete = pathsToDelete[i];
-      loader.text = `Deleting ${i}/${length} ${chalk.cyan(pathToDelete)}`;
-      deletePathOrFile(pathToDelete, {printInfo: false, printError: true, printWarning: false});
+    if (pathsToDelete.length == 0) {
+        console.log("[*] Nothing to delete.");
+    } else {
+        const length = pathsToDelete.length;
+        console.log(chalk.cyan(`'Deleting ${length==1?'path':'paths'}:  [${pathsToDelete.join(',')}]`));
+        for (let i = 0; i < length; i++) {
+            const pathToDelete = pathsToDelete[i];
+            deletePathOrFile(pathToDelete, {printInfo: false, printError: true, printWarning: false});
+        }
     }
-    loader.stop();
-  }
 })();
